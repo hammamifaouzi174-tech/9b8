04956f906c764cab1677a61472c18f1d98b08fe8
hammamifaouzi174-tech <hammamifaouzi174@gmail.com>
2025-10-20 18:39:12 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Rim")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Rim et Adem ")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Adem ")
+})
+basic.showString("Ampoule ")
+basic.forever(function () {
+	
+})
